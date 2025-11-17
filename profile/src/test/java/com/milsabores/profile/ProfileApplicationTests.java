840e5372c33d5148b04dde0c1cda5bb5c47c7a03
Your Name <your.email@example.com>
2025-11-16 23:39:37 -0300

    chore: initial Spring Boot setup

---
+++ profile/src/test/java/com/milsabores/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milsabores.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
