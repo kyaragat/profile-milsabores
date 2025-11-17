a35ece28667881b599a6b5e6514cc474af6388aa
Your Name <your.email@example.com>
2025-11-17 00:06:54 -0300

    feat: add authentication DTOs

---
+++ profile/src/main/java/com/milsabores/profile/dto/RegisterRequest.java
@@ -0,0 +1,11 @@
+package com.milsabores.profile.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
